8361ea8cf6a35c98b508f60a9623c182c6bf0162
Yan Zhang <yan.zhang@insureon.com>
2017-08-25 14:40:27 -0500

    add base project and dashboard controller

---
+++ WebAssetManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAssetManager.Startup))]
+namespace WebAssetManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
